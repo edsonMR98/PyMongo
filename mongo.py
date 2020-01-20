dd037ad8f02cf834d7993942c9f7c71e2fc4e36e
Edson Moreno <edsonmorenoruiz@gmail.com>
2020-01-20 12:57:12 -0600

    Gets all and matched documents

--- mongo.py
+++ mongo.py
@@ -13,13 +13,26 @@ collection = db.test
     ]
 )"""
 
-id = collection.insert_one({"name": "CDMX", "phone": "3141093387", "date": datetime.datetime.utcnow()}).inserted_id # Gets the _id inserted
+id = collection.insert_one({"name": "CDMX", "phone": "3141093387", "date": datetime.datetime.utcnow()}).inserted_id # Gets the _id inserted (1 document)
 print(id) # Prints its ObjectId (ObjectId type)
 idStr = str(id)
 
+ids = collection.insert_many(
+    [
+        {"name": "Colima", "phone": "3141093387", "date": datetime.datetime.utcnow()},
+        {"name": "Manza", "phone": "3141093387", "date": datetime.datetime.utcnow()}
+    ]
+)
+
 #print(db.list_collection_names()) # Gets the collection names
-print(collection.find_one()) # Gets the first document
-print(collection.find_one({"_id": id})) # Gets the matched document
-print(collection.find_one({"_id": idStr})) # Gets the matched document, idStr is str type and there are not a str type _id, Result = None
-print(collection.find_one({"_id": ObjectId(idStr)})) # Gets the matched document, parse a str to ObjectId
-print(collection.find_one({"name": "Yosa"})) # Gets the matched document
+#print(collection.find_one()) # Gets the first document
+#print(collection.find_one({"_id": id})) # Gets the matched document
+#print(collection.find_one({"_id": idStr})) # Gets the matched document, idStr is str type and there are not a str type _id, Result = None
+#print(collection.find_one({"_id": ObjectId(idStr)})) # Gets the matched document, parse a str to ObjectId
+#print(collection.find_one({"name": "Yosa"})) # Gets the matched document
+print(ids.inserted_ids) # gets the _id inserted (2+ documets)
+for doc in collection.find(): # Prints all the collection docuemnts
+    print(doc)
+
+for doc in collection.find({"name": "CDMX"}): # Prints all collections documents matched
+    print(doc)
